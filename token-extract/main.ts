ccd53fb75662697e46e43d5405a543646597feaf
Nils Martel <nilsmartel@yahoo.de>
2025-08-15 21:35:50 +0200

    Prepare outliers for further analysis

--- token-extract/main.ts
+++ token-extract/main.ts
@@ -2,10 +2,14 @@ import { getRawInputLines, getTokensUnclean } from "./extract-tokens";
 
 const input = getRawInputLines();
 
+function isOddLine(line: string) {
+  return line.includes(":");
+}
+
 for (let line of input) {
   let tokens = getTokensUnclean(line);
   if (tokens.length > 0) {
     //console.log(tokens.join(" "));
-    console.log(line);
+    if (isOddLine(line)) console.log(line);
   }
 }
