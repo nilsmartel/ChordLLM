c63951f5308746902c80f82eebd71c7c833b07b0
Nils Martel <nilsmartel@yahoo.de>
2025-09-15 10:44:17 +0200

    Implement cli argument parsing

--- token-extract/main.ts
+++ token-extract/main.ts
@@ -7,7 +7,18 @@ import {
 } from "./extract-tokens";
 import { TokenType, getSymbolType } from "./token-type";
 
-const input = getRawInputLines();
+function getFilename(): string {
+  let filepath = process.argv[2];
+  if (filepath == null || filepath == "--help") {
+    console.log("expected first argument to be filepath or - for stdin.");
+    process.exit(1);
+  }
+  if (filepath == "-") filepath = "/dev/stdin";
+
+  return filepath;
+}
+
+const input = getRawInputLines(getFilename());
 
 for (let line of input) {
   let tokens = verifyAndCleanLine(getTokensUnclean(line));
