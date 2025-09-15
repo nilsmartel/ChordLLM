fbacea2793f933201cca54fe401078fbf901f484
Nils Martel <nilsmartel@yahoo.de>
2025-09-15 10:37:41 +0200

    Refactor

--- token-extract/extract-tokens.ts
+++ token-extract/extract-tokens.ts
@@ -136,3 +136,26 @@ export function getTokensUnclean(line: string): string[] {
 
   return rawTokens;
 }
+
+export function verifyAndCleanLine(line: string[]): string[] | null {
+  if (line.length == 1 && line[0] == START_OF_SONG) return line;
+  let symbols = line.map(getSymbolType);
+
+  let newLine: string[] = [];
+  for (let i in line) {
+    let s = symbols[i];
+    // Ignore lines with Descriptions of chords
+    if (s == TokenType.ChordDesc) return null;
+    // Another option would be to just keep the repeat symbol.
+    // but I think it would feel odd to get that from a chord completion model.
+    if (s == TokenType.Repeat) continue;
+    if (s == TokenType.Noise) continue;
+    if (s == TokenType.Speech) continue;
+
+    let token = line[i];
+    newLine.push(token);
+  }
+
+  if (newLine.length == 0) return null;
+  return newLine;
+}
--- token-extract/main.ts
+++ token-extract/main.ts
@@ -3,33 +3,11 @@ import {
   getTokensUnclean,
   START_OF_SONG,
   transformToken,
+  verifyAndCleanLine,
 } from "./extract-tokens";
 import { TokenType, getSymbolType } from "./token-type";
 
 // TODO end of song tokens
-
-function verifyAndCleanLine(line: string[]): string[] | null {
-  if (line.length == 1 && line[0] == START_OF_SONG) return line;
-  let symbols = line.map(getSymbolType);
-
-  let newLine: string[] = [];
-  for (let i in line) {
-    let s = symbols[i];
-    // Ignore lines with Descriptions of chords
-    if (s == TokenType.ChordDesc) return null;
-    // Another option would be to just keep the repeat symbol.
-    // but I think it would feel odd to get that from a chord completion model.
-    if (s == TokenType.Repeat) continue;
-    if (s == TokenType.Noise) continue;
-    if (s == TokenType.Speech) continue;
-
-    let token = line[i];
-    newLine.push(token);
-  }
-
-  if (newLine.length == 0) return null;
-  return newLine;
-}
 
 const input = getRawInputLines();
 
