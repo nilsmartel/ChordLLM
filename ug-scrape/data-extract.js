6985c4d2f2a1128afd1281a1740f3e52b3879efe
Nils Martel <nilsmartel@yahoo.de>
2025-09-16 09:34:50 +0200

    Implement extracting JS Data from UG Html

--- ug-scrape/data-extract.js
+++ ug-scrape/data-extract.js
@@ -9,3 +9,12 @@ function unsanitze(s) {
     .replace(/&#39;/g, "'")
     .replace(/&nbsp;/g, " ");
 }
+
+export function extractDataContent(s) {
+  // extract content between data-content" and the next " (string delimiter)
+  let dataContent = s.split('data-content="')[1].split('"')[0];
+  // unsanitize content
+  let json = unsanitze(dataContent);
+  // it's in json format, so we can work with a js object here
+  return JSON.parse(json);
+}
