db69859df3bde5cd814faeb1cb90f458b9a7173a
Nils Martel <nilsmartel@yahoo.de>
2025-09-16 09:34:10 +0200

    Write function to unsanitize data coming from Ultimate Guitar

---
+++ ug-scrape/data-extract.js
@@ -0,0 +1,11 @@
+//
+// file contains functions to extract data from ug page
+function unsanitze(s) {
+  return s
+    .replace(/&quot;/g, '"')
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&#39;/g, "'")
+    .replace(/&nbsp;/g, " ");
+}
