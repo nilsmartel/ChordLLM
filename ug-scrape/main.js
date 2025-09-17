8ce727e33d5de1b15cb16608100b5dc12b8de015
Nils Martel <nilsmartel@yahoo.de>
2025-09-17 15:17:05 +0200

    Save chord sheets

--- ug-scrape/main.js
+++ ug-scrape/main.js
@@ -3,6 +3,10 @@ async function main() {
     for await (const u of songUrls(1, 1000)) {
       let html = await axios.get(u);
       let res = extractChordSheet(html.data);
+      let fname = u.split("tab/")[1].replaceAll("/", "--");
+      fs.writeFile("./output/" + fname, res, () => {
+        console.log(fname);
+      });
     }
   } catch (error) {
     console.error("Error:", error.message);
